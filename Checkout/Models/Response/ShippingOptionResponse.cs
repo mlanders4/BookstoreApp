16545890d1cd780543bf760cdb6cc3b77dfde602
mlanders4 <142841092+mlanders4@users.noreply.github.com>
2025-04-18 17:16:15 -0500

    Update ShippingOptionResponse.cs

--- Checkout/Models/Response/ShippingOptionResponse.cs
+++ Checkout/Models/Response/ShippingOptionResponse.cs
@@ -1,1 +1,8 @@
+// Checkout/Models/Responses/ShippingOption.cs
+public record ShippingOptionResponse(
+    ShippingMethod Method,
+    decimal Cost,
+    string DeliveryEstimate
+);
 
+public enum ShippingMethod { Standard, Express, SameDay }
