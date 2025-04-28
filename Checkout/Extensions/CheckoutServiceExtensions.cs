2ed3db61d4eee319b481192e8c72f3d4c22d16ad
mlanders4 <142841092+mlanders4@users.noreply.github.com>
2025-04-28 13:29:14 -0500

    Update CheckoutServiceExtensions.cs

--- Checkout/Extensions/CheckoutServiceExtensions.cs
+++ Checkout/Extensions/CheckoutServiceExtensions.cs
@@ -1,1 +1,23 @@
+using Microsoft.Extensions.DependencyInjection;
+using Bookstore.Checkout.Engine;
+using Bookstore.Checkout.Accessors;
+using Bookstore.Checkout.Contracts;
 
+namespace Bookstore.Checkout.Extensions
+{
+    public static class CheckoutServiceExtensions
+    {
+        public static IServiceCollection AddCheckoutServices(this IServiceCollection services)
+        {
+            // Register all Checkout services
+            services.AddScoped<ICheckoutService, CheckoutEngine>();
+            services.AddScoped<IPaymentValidator, PaymentValidator>();
+            services.AddScoped<IShippingCalculator, ShippingCalculator>();
+            services.AddScoped<IOrderAccessor, OrderAccessor>();
+            services.AddScoped<IPaymentAccessor, PaymentAccessor>();
+            services.AddScoped<IShippingAccessor, ShippingAccessor>();
+            
+            return services;
+        }
+    }
+}
