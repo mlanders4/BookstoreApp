d518b1a75e5af171e62bab2b181602343765a1bd
mlanders4 <142841092+mlanders4@users.noreply.github.com>
2025-04-30 15:42:54 -0500

    Update CheckoutException.cs

--- Checkout/Core/Exceptions/CheckoutException.cs
+++ Checkout/Core/Exceptions/CheckoutException.cs
@@ -1,1 +1,13 @@
+namespace Bookstore.Checkout.Core.Exceptions
+{
+    public class CheckoutException : Exception
+    {
+        public string ErrorCode { get; }
 
+        public CheckoutException(string errorCode, string message = null)
+            : base(message ?? "Checkout error occurred") 
+        {
+            ErrorCode = errorCode;
+        }
+    }
+}
