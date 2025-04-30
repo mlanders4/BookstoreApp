b19ea7248de00e9ced7110b6e338e020ef7b9e1e
mlanders4 <142841092+mlanders4@users.noreply.github.com>
2025-04-30 15:40:01 -0500

    Update CheckoutDbContext.cs

--- Checkout/Data/CheckoutDbContext.cs
+++ Checkout/Data/CheckoutDbContext.cs
@@ -1,1 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Bookstore.Checkout.Data.Entities;
 
+namespace Bookstore.Checkout.Data
+{
+    public class CheckoutDbContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Shipping> Shippings { get; set; }
+
+        public CheckoutDbContext(DbContextOptions<CheckoutDbContext> options) 
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure composite keys or relationships here
+            modelBuilder.Entity<Order>()
+                .HasMany(o => o.Items)
+                .WithOne()
+                .HasForeignKey(i => i.OrderId);
+        }
+    }
+}
