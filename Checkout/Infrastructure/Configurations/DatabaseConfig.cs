d0a7870a571fc626fd0bd73320976ede42ba352a
mlanders4 <142841092+mlanders4@users.noreply.github.com>
2025-04-30 17:03:28 -0500

    Update DatabaseConfig.cs

--- Checkout/Infrastructure/Configurations/DatabaseConfig.cs
+++ Checkout/Infrastructure/Configurations/DatabaseConfig.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Polly;
 
 namespace Bookstore.Checkout.Infrastructure.Configurations
 {
@@ -8,9 +9,17 @@ namespace Bookstore.Checkout.Infrastructure.Configurations
         public static void ConfigureDbContext(IServiceCollection services, IConfiguration config)
         {
             services.AddDbContext<CheckoutDbContext>(options =>
+            {
                 options.UseSqlServer(
                     config.GetConnectionString("CheckoutDB"),
-                    sqlOptions => sqlOptions.EnableRetryOnFailure()));
+                    sqlOptions => 
+                    {
+                        sqlOptions.EnableRetryOnFailure(
+                            maxRetryCount: 5,
+                            maxRetryDelay: TimeSpan.FromSeconds(30),
+                            errorNumbersToAdd: null);
+                    });
+            });
         }
     }
 }
