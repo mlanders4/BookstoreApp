e24a7e9f875713b5aacde8c802acc42a3b46eb6c
mlanders4 <142841092+mlanders4@users.noreply.github.com>
2025-04-30 17:23:00 -0500

    Update Payment.cs

--- Checkout/Data/Entities/Payment.cs
+++ Checkout/Data/Entities/Payment.cs
@@ -1,14 +1,28 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Bookstore.Checkout.Data.Entities
 {
     public class Payment
     {
+        [Column("checkout_id")]
         public int Id { get; set; }
-        public int OrderId { get; set; }
+
+        [Column("order_id")]
+        public Guid OrderId { get; set; }
+
+        [Column("credit_card_number")]
         public string MaskedCardNumber { get; set; }
+
+        [Column("expiry_date")]
+        public DateTime ExpiryDate { get; set; }
+
+        [Column("amount")]
         public decimal Amount { get; set; }
-        public string Status { get; set; }
-        
-        // Navigation
+
+        [Column("payment_status")]
+        public string Status { get; set; } = "pending";
+
         public Order Order { get; set; }
     }
 }
